9cebc8509657c2c7ba138cdeaa7dba8cd20e8b10
DiZZert <dizzertmaster@gmail.com>
2021-05-28 20:07:55 +0900

    another js file

---
+++ src/gui_layer.js
@@ -0,0 +1,29 @@
+let info;
+
+class Example extends Phaser.Scene
+{
+    constructor ()
+    {
+        super();
+    }
+
+    preload ()
+    {
+
+    }
+
+    create ()
+    {
+        info.setText('Test test');
+    }
+
+    update ()
+    {
+
+    }
+
+    clickHandler (box)
+    {
+
+    }
+}
