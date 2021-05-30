88f8d49ab5703c3290420ec61eaf62d4c6b8bf55
DiZZert <dizzertmaster@gmail.com>
2021-05-30 17:47:05 +0300

    shit

--- tmp/click.js
+++ tmp/click.js
@@ -1,7 +1,33 @@
-import { markPlugin } from './doMark.js';
-
 let info;
 let clickCount = 0;
+
+class markGameObject extends Phaser.GameObjects.Image {
+    constructor (scene, x, y)
+    {
+        super(scene, x, y, 'mark');
+        this.setScale(0.1);
+        // this.setInteractive();
+        // this.on('clicked', this.clickHandler, this);
+        // clickCount++;
+        // this.input.on('gameobjectup', function (pointer, gameObject)
+        // {
+        //     gameObject.emit('clicked', gameObject);
+        // }, this);
+    }
+}
+
+class markPlugin extends Phaser.Plugins.BasePlugin {
+    constructor (pluginManager)
+    {
+        super(pluginManager);
+        pluginManager.registerGameObject('mark', this.createMark);
+    }
+
+    createMark (x, y)
+    {
+        return this.displayList.add(new markGameObject(this.scene, x, y));
+    }
+}
 
 class marksMap extends Phaser.Scene
 {
