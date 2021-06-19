087c833340b5dd9e29dbd75c22a1a729f3f59a92
DiZZert <dizzertmaster@gmail.com>
2021-06-19 15:48:19 +0300

    change

--- tmp/wheel.js
+++ tmp/wheel.js
@@ -1,28 +1,24 @@
 ( function( $ ) {
 
-	const $wheel = $( '.wheel .wheel__inner' );
-	const items = 28;
-	const diameter = $wheel.height();
-	const radius = diameter / 2;
-	const angle = 360 / items;
-	const circumference = Math.PI * diameter;
-	const height = circumference / items;
+    const $wheel = $( '.wheel .wheel__inner' );
+    const items = 28;
+    const diameter = $wheel.height();
+    const radius = diameter / 2;
+    const angle = 360 / items;
+    const circumference = Math.PI * diameter;
+    const height = circumference / items;
 
-	$wheel.css( {
-		'transform-origin': `50% calc(50% + ${height / 2}px)`,
-		'margin-top': `-${height}px`
-	} );
+    for ( let i = 0; i < items; i++ ) {
+        var transform = `rotateX(${ angle * i }deg) translateZ(${ radius }px)`;
 
-	for ( let i = 0; i < items; i++ ) {
-		var transform = `rotateX(${ angle * i }deg) translateZ(${ radius }px)`;
-
-		$( '<div>', {
-			class: 'wheel__segment'
-		} ).css( {
-			'transform': transform,
-			'height': height,
-		} ).html( `<span>Item ${ i }</span>` ).appendTo( $wheel );
-
-	}
+        $( '<div>', {
+            class: 'wheel__segment'
+        } ).css( {
+            'transform': transform,
+            'height': height,
+        } ).html( `<span>Item ${ i }</span>` ).appendTo( $wheel );
+    }
+  $wheel.css('transform-origin','50% calc(50% + '+height/2+'px)');
+  $wheel.css('margin-top','-'+height+'px'); /* negative margin here to keep the element into the center */
 
 } )( jQuery );
