95c8d38c852fa04410d037f15ad47c2aeb5553cd
DiZZert <dizzertmaster@gmail.com>
2021-07-11 13:16:26 +0900

    subday functional page

--- scripts/subday.js
+++ scripts/subday.js
@@ -2,12 +2,29 @@
 
   var ifr = document.getElementById("wheelFrame");
 
-  $( ".twheel_button input[type=submit]" ).click(function( event ) {
-    ifr.contentWindow.location.replace("sdtype-wheel.html");
+  $(function(){
+    $(".twheel_button").on("click", function(){
+        ifr.contentWindow.location.replace("sdtype-wheel.html");
+    });
   });
 
-  $( ".ewheel_button input[type=submit]" ).click(function( event ) {
-    ifr.contentWindow.location.replace("effects-wheel.html");
+
+  $(function(){
+    $(".ewheel_button").on("click", function(){
+        ifr.contentWindow.location.replace("effects-wheel.html");
+    });
   });
+
+  // $( ".twheel_button area[shape=rect]" ).click(function( event ) {
+  //   ifr.contentWindow.location.replace("sdtype-wheel.html");
+  // });
+  //
+  // $( ".ewheel_button input[type=submit]" ).click(function( event ) {
+  //   ifr.contentWindow.location.replace("effects-wheel.html");
+  // });
+
+  function frameType(){
+    console.log("type");
+  }
 
 } )( jQuery );
