3be90c0f29b672edb36eb0df9ebe278b7e584e65
DiZZert <dizzertmaster@gmail.com>
2021-07-11 13:18:58 +0900

    вынес мусор

--- scripts/subday.js
+++ scripts/subday.js
@@ -8,23 +8,10 @@
     });
   });
 
-
   $(function(){
     $(".ewheel_button").on("click", function(){
         ifr.contentWindow.location.replace("effects-wheel.html");
     });
   });
-
-  // $( ".twheel_button area[shape=rect]" ).click(function( event ) {
-  //   ifr.contentWindow.location.replace("sdtype-wheel.html");
-  // });
-  //
-  // $( ".ewheel_button input[type=submit]" ).click(function( event ) {
-  //   ifr.contentWindow.location.replace("effects-wheel.html");
-  // });
-
-  function frameType(){
-    console.log("type");
-  }
 
 } )( jQuery );
