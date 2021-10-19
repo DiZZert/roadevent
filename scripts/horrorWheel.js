e9ac1c69729bc6f98d33aa40d06c8fe72b25c40e
DiZZert <dizzertmaster@gmail.com>
2021-10-19 23:30:50 +0900

    icons and horror sounds

--- scripts/horrorWheel.js
+++ scripts/horrorWheel.js
@@ -1,29 +1,29 @@
 ( function( $ ) {
 
   var effectsArray =
-  '[{"type": "buff","name": "Да ваще не страшно","pic": "test.png","description": "при каждом твоем крике - сабка"},'+
-  '{"type": "buff","name": "Откукда звуки?!","pic": "test.png","description": "семплы со скримерами"},'+
-  '{"type": "buff","name": "Ну нахер","pic": "test.png","description": "повышение градуса самого хоррора (от пусеченых до самых страшных)"},'+
-  '{"type": "buff","name": "В бездну список","pic": "test.png","description": "плюс игра, минус игра"},'+
-  '{"type": "buff","name": "Повелитель ночи","pic": "test.png","description": "выключить свет"},'+
-  '{"type": "buff","name": "Пьянка с ебаками","pic": "test.png","description": "алко обсёр (додумать) выпивать за каждый напуг"},'+
-  '{"type": "buff","name": "Супер сус","pic": "test.png","description": "сидеть какое то время с фанариком у подбородка (додумать мб?)"},'+
-  '{"type": "buff","name": "140 BPM","pic": "test.png","description": "Пульсометр (превысил какое значение че то делаешь) - крутится колесо, но это может произойти только один раз за стрим."},'+
-  '{"type": "buff","name": "empty","pic": "test.png","description": "кто больше всего раз крикнет или явно испугается дарит 50 сабок на канал победителя (в случае если коллаб ивент)"},'+
-  '{"type": "buff","name": "Не хочу умирац","pic": "test.png","description": "Следующая игра ролится из категории супер страшные"},'+
-  '{"type": "buff","name": "Outlast","pic": "test.png","description": "Временно накатить на вебку фильтр ночного виденья"},'+
-  '{"type": "buff","name": "Welcome to the family son","pic": "test.png","description": "страшные алерты"},'+
-  '{"type": "buff","name": "empty","pic": "test.png","description": "прикол с громкостью (?)"},'+
-  '{"type": "buff","name": "Коричневые паруса","pic": "test.png","description": "пойти сменить труханы и показать на стриме сами труханы без себя"},'+
-  '{"type": "buff","name": "200 IQ","pic": "test.png","description": "если не можешь пройти загадку за 5 мин, то пьешь че-то или что-то еще"},'+
-  '{"type": "buff","name": "(\/) MLG (\/)","pic": "test.png","description": "Что-то за геймоверы"},'+
-  '{"type": "buff","name": "Судьба","pic": "test.png","description": "Переход через категорию. Если и вы и так на максимальной категории, то воля случая. Орел - плюс игры; Решка - повезло."},'+
-  '{"type": "buff","name": "Повезло повезло","pic": "test.png","description": "Понижение категории"},'+
-  '{"type": "buff","name": "Стабильность","pic": "test.png","description": "следующая игра будет на текущей категории"},'+
-  '{"type": "buff","name": "Есть два стула","pic": "test.png","description": "Стримерский стул меняется на табуретку. Если стример упал с табуретки от испуга - что-то?"},'+
-  '{"type": "buff","name": "Киновечер","pic": "test.png","description": "с фильмом ужасов (выбор из списка, заказанного зрителями)"},'+
-  '{"type": "legendary","name": "empty","pic": "test.png","description": ""},'+
-  '{"type": "debuff","name": "empty","pic": "test.png","description": "Следующие 3 барабана будут по фикс прайсу"}]';
+  '[{"type": "buff","name": "Да ваще не страшно","pic": "icon001.png","description": "при каждом твоем крике - сабка"},'+
+  '{"type": "buff","name": "Откукда звуки?!","pic": "icon005.png","description": "семплы со скримерами"},'+
+  '{"type": "buff","name": "Ну нахер","pic": "icon003.png","description": "повышение градуса самого хоррора (от пусеченых до самых страшных)"},'+
+  '{"type": "buff","name": "В бездну список","pic": "icon004.png","description": "плюс игра, минус игра"},'+
+  '{"type": "buff","name": "Повелитель ночи","pic": "icon002.png","description": "выключить свет"},'+
+  '{"type": "buff","name": "Пьянка с ебаками","pic": "icon006.png","description": "алко обсёр (додумать) выпивать за каждый напуг"},'+
+  '{"type": "buff","name": "Супер сус","pic": "icon007.png","description": "сидеть какое то время с фанариком у подбородка (додумать мб?)"},'+
+  '{"type": "buff","name": "140 BPM","pic": "icon008.png","description": "Пульсометр (превысил какое значение че то делаешь) - крутится колесо, но это может произойти только один раз за стрим."},'+
+  '{"type": "buff","name": "empty","pic": "icon009.png","description": "кто больше всего раз крикнет или явно испугается дарит 50 сабок на канал победителя (в случае если коллаб ивент)"},'+
+  '{"type": "buff","name": "Не хочу умирац","pic": "icon010.png","description": "Следующая игра ролится из категории супер страшные"},'+
+  '{"type": "buff","name": "Outlast","pic": "icon011.png","description": "Временно накатить на вебку фильтр ночного виденья"},'+
+  '{"type": "buff","name": "Welcome to the family son","pic": "icon012.png","description": "страшные алерты"},'+
+  '{"type": "buff","name": "empty","pic": "icon013.png","description": "прикол с громкостью (?)"},'+
+  '{"type": "buff","name": "Коричневые паруса","pic": "icon014.png","description": "пойти сменить труханы и показать на стриме сами труханы без себя"},'+
+  '{"type": "buff","name": "200 IQ","pic": "icon015.png","description": "если не можешь пройти загадку за 5 мин, то пьешь че-то или что-то еще"},'+
+  '{"type": "buff","name": "(\/) MLG (\/)","pic": "icon016.png","description": "Что-то за геймоверы"},'+
+  '{"type": "buff","name": "Судьба","pic": "icon017.png","description": "Переход через категорию. Если и вы и так на максимальной категории, то воля случая. Орел - плюс игры; Решка - повезло."},'+
+  '{"type": "buff","name": "Повезло повезло","pic": "icon018.png","description": "Понижение категории"},'+
+  '{"type": "buff","name": "Стабильность","pic": "icon019.png","description": "следующая игра будет на текущей категории"},'+
+  '{"type": "buff","name": "Есть два стула","pic": "icon020.png","description": "Стримерский стул меняется на табуретку. Если стример упал с табуретки от испуга - что-то?"},'+
+  '{"type": "buff","name": "Киновечер","pic": "icon021.png","description": "с фильмом ужасов (выбор из списка, заказанного зрителями)"},'+
+  '{"type": "buff","name": "empty","pic": "legend_icon.png","description": ""},'+
+  '{"type": "buff","name": "empty","pic": "icon004.png","description": "Следующие 3 барабана будут по фикс прайсу"}]';
   ;
 
   var spinSounArray = [
@@ -58,7 +58,7 @@
 
     function playSound(name) {
       var soundFile = document.createElement("audio");
-      soundFile.src = "src/audio/" + name + ".mp3";
+      soundFile.src = "src/audio/horror/" + name + ".mp3";
       soundFile.play();
     }
 
@@ -78,7 +78,7 @@
 
           var transform = `rotateY(${ angle * i }deg) translateZ(${ radius }px)`;
           $( '<div>', {class: 'wheel__segment'} )
-          .html( `<span> <img src="src/assets/wheel_icons/effects/` + jsonObject[i].pic + `" width="60" height="60"> </span>` ).appendTo( $wheel )
+          .html( `<span> <img src="src/assets/wheel_horror/icons/` + jsonObject[i].pic + `" width="60" height="60"> </span>` ).appendTo( $wheel )
           .css( {'transform': transform,'height': height, 'width': width, 'background-image': 'url(src/assets/wheel_img/'+jsonObject[i].type+'.png)'} )
 
       }
@@ -108,12 +108,12 @@
             var transform = `rotateY(${ angle * i }deg) translateZ(${ radius }px)`;
 
             $( '<div>', {class: 'wheel__segment'} )
-            .html( `<span> <img src="src/assets/wheel_icons/effects/` + jsonObject[i].pic + `" width="60" height="60"> </span>` ).appendTo( $wheel )
+            .html( `<span> <img src="src/assets/wheel_horror/icons/` + jsonObject[i].pic + `" width="60" height="60"> </span>` ).appendTo( $wheel )
             .css( {'transform': transform,'height': height, 'width': width, 'background-image': 'url(src/assets/wheel_img/'+jsonObject[i].type+'.png)'} )
             .click(function() {
 
               deletedEffects.push(jsonObject[i]);
-              playSound("spank");
+              playSound("crow");
               console.log(jsonObject[i]);
             });
         }
@@ -127,7 +127,7 @@
         $("#descriptionText").text('');
         $("#descriptionLegendary").text('');
 
-        playSound("spinning/" + spinSounArray[Math.floor(getRandom(spinSounArray.length,0))]);
+        // playSound("spinning/" + spinSounArray[Math.floor(getRandom(spinSounArray.length,0))]);
 
         $wheelSpinClass.addClass('wheelAnimation');
 
@@ -147,9 +147,9 @@
             $("#descriptionText").text(jsonObject[roundedPosition].description);
 
             if(jsonObject[roundedPosition].type == "debuff") {
-              playSound("sorry");
+              // playSound("sorry");
             } else if (jsonObject[roundedPosition].type == "legendary") {
-              playSound("legendary");
+              // playSound("legendary");
 
               let legendaryPosition = Math.floor(getRandom(horrorsCategoryArray.length, 0))
               $("#descriptionName").text('');
@@ -162,7 +162,7 @@
               }, 2000);
 
             } else {
-              playSound("what_you_see");
+              playSound("thunder");
             }
 
           }, 9300);
